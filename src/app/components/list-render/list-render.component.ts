a5da1cdc9b83e6e51298ff3fb9c863c1c510d748
Csf32 <c.souzaf@hotmail.com>
2023-02-10 10:01:50 -0300

    Interface and pipes

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -7,9 +9,20 @@ import { Component } from '@angular/core';
 })
 export class ListRenderComponent {
 
-  animals = [
-    {name: "Alef", type:"Horse"},
-    {name: "Bigode", type:"Dog"},
-    {name: "Odin", type:"Cat"}
+  animals: Animal[] = [
+    {name: "Alef", type:"Horse", age: 4},
+    {name: "Bigode", type:"Dog", age: 9},
+    {name: "Odin", type:"Cat", age: 2}
   ]
+
+  animalDetails = ''
+
+  animal: Animal ={
+    name: "Teste",
+    type: "Someone",
+    age: 10,
+  }
+  showAge(animal: Animal):void {
+    this.animalDetails = `O animal ${animal.name} tem ${animal.age} anos`
+  }
 }
